9ae6f48d709500888378a0fa40dac2625fb2c5cc
s123tuti <stutisharmaofficial@gmail.com>
2023-10-24 11:35:35 +0530

    components

---
+++ src/components/taskList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./tasklist.css"
+
+function TaskList({ tasks, onUpdateTask, onDeleteTask }) {
+  return (
+    <ul className="task-list">
+      {tasks.map((task) => (
+          <li key={task.id}>
+          <h4>{task.task}</h4>
+          <p>{task.description}</p>
+          <p>Current-Status: {task.status}</p>
+          <div>
+          <button onClick={() => onUpdateTask(task.id, { status: "done" })}>
+           Done
+          </button>
+          <button onClick={() => onUpdateTask(task.id, { status: "in Progress" })}>
+            Move to In Progress
+          </button>
+          <button onClick={() => onUpdateTask(task.id, { status: "pending" })}>
+            Move to Pending
+          </button>
+          <button onClick={() => onUpdateTask(task.id, { status: "completed" })}>
+            Move to Completed
+          </button>
+        <button style={{backgroundColor:"red"}} onClick={() => onDeleteTask(task.id)}>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  
+  );
+}
+
+  
+
+export default TaskList;
